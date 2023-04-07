e92ab298615cf9a8dd25399170457a65cf343595
Jonathan Stock <jonathanhstock@gmail.com>
2023-04-07 16:24:49 -0700

    Updated FAQ and About

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -18,12 +18,11 @@ const Manage = () => {
   const { user } = useSelector((state) => state.auth);
 
   const stmts = [
-    ["Our Mission Statement", "answer"],
-    ["About the Project", "answer"],
-    ["Our Commitment", "answer"],
-    ["Santa Clara Mission Statement", "answer"],
-    ["More Information", "github, research doc, etc"],
-    ["Special Thanks to", "advisors..."],
+    ["Our Mission Statement", "Our motivation for this project is to ensure that bees are protected as their role to the environment is of utmost importance to the sustainability of our ecosystems. Our key principle for this project is to help beekeepers maintain their role as caretakers for the bees and hive by providing solutions that can be easily implemented and used in their field."],
+    ["Our Commitment", "As students of the university, we are committed to providing an uncompromising standard of support to this project through learning, creativity, and development of skills in order to ensure the utmost quality of work is provided."],
+    ["Santa Clara Mission Statement", "The University pursues its vision by creating an academic community that educates the whole person within the Jesuit, Catholic tradition, making student learning our central focus, continuously improving our curriculum and co-curriculum, strengthening our scholarship and creative work, and serving the communities of which we are a part in Silicon Valley and around the world."],
+    ["More Information", "Github: https://github.com/paizcollin/beehive-webapp Research Document: https://drive.google.com/file/d/1Ble9ErteABF_nTk1ISFajrWrM6_Eq5KN/view?usp=sharing"],
+    ["Special Thanks to", "Gerhard and Lisa Eschelbeck, Wendy Mather, and Kian Nikzad"],
   ];
 
   useEffect(() => {
